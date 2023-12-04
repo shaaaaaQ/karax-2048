dae193ba121515668abe7678ff416590bb356015
sh <64454953+shaaaaaQ@users.noreply.github.com>
2023-12-04 19:42:20 +0900

    watch toka

--- src/index.nim
+++ src/index.nim
@@ -1,31 +1,25 @@
 include karax / prelude
 
-proc tile(value: int): VNode =
+var board = @[
+  @[2, 0, 0, 16],
+  @[0, 4, 0, 0],
+  @[0, 0, 0, 0],
+  @[0, 0, 0, 8]
+]
+
+proc createTile(value: int): VNode =
   result = buildHtml(tdiv(class="w-[100px] h-[100px] text-center font-bold text-[30px] leading-[100px] bg-sky-200")):
       if value != 0:
         text($value)
 
-proc board(): VNode =
+proc createBoard(): VNode =
   result = buildHtml(tdiv(class="w-fit grid grid-cols-4 gap-2")):
-    tile(0)
-    tile(2)
-    tile(16)
-    tile(4)
-    tile(0)
-    tile(16)
-    tile(4)
-    tile(2)
-    tile(16)
-    tile(0)
-    tile(2)
-    tile(16)
-    tile(4)
-    tile(2)
-    tile(0)
-    tile(16)
+    for row in board:
+      for value in row:
+        createTile(value)
 
 proc createDom(): VNode =
   result = buildHtml(tdiv):
-    board()
+    createBoard()
 
 setRenderer(createDom)
