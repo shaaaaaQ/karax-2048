e71aab7cda1d163efdbcf44fac596445110d89e5
sh <64454953+shaaaaaQ@users.noreply.github.com>
2023-12-05 17:39:18 +0900

    keyboard

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 nim wakaran
-keyboard event douyaruno
--- src/index.nim
+++ src/index.nim
@@ -1,12 +1,14 @@
 import std/random
 include karax / prelude
+import karax / kdom
 
-var board = @[
-  @[2, 0, 0, 16],
-  @[0, 4, 0, 0],
-  @[0, 0, 0, 0],
-  @[0, 0, 0, 8]
-]
+type Direction = enum
+  Left
+  Down
+  Up
+  Right
+
+var board: seq[seq[int]]
 
 proc countEmptyTile(): int =
   result = 0
@@ -18,6 +20,7 @@ proc countEmptyTile(): int =
 proc addTile() =
   let emptyTileNum = countEmptyTile()
   if emptyTileNum != 0:
+    randomize()
     let index = rand(1..emptyTileNum)
     var count = 0
     for y, row in board:
@@ -30,80 +33,88 @@ proc addTile() =
             else:
               board[y][x] = 2
 
-proc moveLeft() =
-  for y in countup(0, 3):
-    for x in countup(0, 3):
-      let value = board[y][x]
-      if value == 0: continue
-      var nx = x
-      while true:
-        dec(nx)
-        if nx < 0: break
-        elif board[y][nx] == 0:
-          board[y][nx+1] = 0
-          board[y][nx] = value
-        elif board[y][nx] == value:
-          board[y][nx+1] = 0
-          board[y][nx] = value * 2
-        else:
-          break
+proc init() =
+  board = @[
+    @[0, 0, 0, 0],
+    @[0, 0, 0, 0],
+    @[0, 0, 0, 0],
+    @[0, 0, 0, 0]
+  ]
+  addTile()
 
-proc moveDown() =
-  for y in countdown(3, 0):
-    for x in countup(0, 3):
-      let value = board[y][x]
-      if value == 0: continue
-      var ny = y
-      while true:
-        echo "loop down"
-        inc(ny)
-        if ny > 3: break
-        elif board[ny][x] == 0:
-          board[ny-1][x] = 0
-          board[ny][x] = value
-        elif board[ny][x] == value:
-          board[ny-1][x] = 0
-          board[ny][x] = value * 2
-        else:
-          break
-
-proc moveUp() =
-  for y in countup(0, 3):
-    for x in countup(0, 3):
-      let value = board[y][x]
-      if value == 0: continue
-      var ny = y
-      while true:
-        echo "loop up"
-        dec(ny)
-        if ny < 0: break
-        elif board[ny][x] == 0:
-          board[ny+1][x] = 0
-          board[ny][x] = value
-        elif board[ny][x] == value:
-          board[ny+1][x] = 0
-          board[ny][x] = value * 2
-        else:
-          break
-
-proc moveRight() =
-  for y in countup(0, 3):
-    for x in countdown(3, 0):
-      let value = board[y][x]
-      if value == 0: continue
-      var nx = x
-      while true:
-        echo "loop right"
-        inc(nx)
-        if nx > 3: break
-        elif board[y][nx] == 0:
-          board[y][nx-1] = 0
-          board[y][nx] = value
-        elif board[y][nx] == value:
-          board[y][nx-1] = 0
-          board[y][nx] = value * 2
-        else:
-          break
+proc move(dir: Direction) =
+  let tmp = board
+  case dir
+  of Direction.Left:
+    for y in countup(0, 3):
+      for x in countup(0, 3):
+        let value = board[y][x]
+        if value == 0: continue
+        var nx = x
+        while true:
+          dec(nx)
+          if nx < 0: break
+          elif board[y][nx] == 0:
+            board[y][nx+1] = 0
+            board[y][nx] = value
+          elif board[y][nx] == value:
+            board[y][nx+1] = 0
+            board[y][nx] = value * 2
+          else:
+            break
+  of Direction.Down:
+    for y in countdown(3, 0):
+      for x in countup(0, 3):
+        let value = board[y][x]
+        if value == 0: continue
+        var ny = y
+        while true:
+          inc(ny)
+          if ny > 3: break
+          elif board[ny][x] == 0:
+            board[ny-1][x] = 0
+            board[ny][x] = value
+          elif board[ny][x] == value:
+            board[ny-1][x] = 0
+            board[ny][x] = value * 2
+          else:
+            break
+  of Direction.Up:
+    for y in countup(0, 3):
+      for x in countup(0, 3):
+        let value = board[y][x]
+        if value == 0: continue
+        var ny = y
+        while true:
+          dec(ny)
+          if ny < 0: break
+          elif board[ny][x] == 0:
+            board[ny+1][x] = 0
+            board[ny][x] = value
+          elif board[ny][x] == value:
+            board[ny+1][x] = 0
+            board[ny][x] = value * 2
+          else:
+            break
+  of Direction.Right:
+    for y in countup(0, 3):
+      for x in countdown(3, 0):
+        let value = board[y][x]
+        if value == 0: continue
+        var nx = x
+        while true:
+          inc(nx)
+          if nx > 3: break
+          elif board[y][nx] == 0:
+            board[y][nx-1] = 0
+            board[y][nx] = value
+          elif board[y][nx] == value:
+            board[y][nx-1] = 0
+            board[y][nx] = value * 2
+          else:
+            break
+  if tmp != board:
+    addTile()
 
 proc renderTile(value: int): VNode =
   result = buildHtml(tdiv(class="w-[100px] h-[100px] text-center font-bold text-[30px] leading-[100px] bg-sky-200")):
@@ -116,31 +127,29 @@ proc renderBoard(): VNode =
       for value in row:
         renderTile(value)
 
-proc renderController(): VNode =
-  result = buildHtml(tdiv):
-    button():
-      text("←")
-      proc onclick() =
-        let tmp = board
-        moveLeft()
-        if tmp != board:
-          addTile()
-    button():
-      text("↓")
-      proc onclick() =
-        moveDown()
-    button():
-      text("↑")
-      proc onclick() =
-        moveUp()
-    button():
-      text("→")
-      proc onclick() =
-        moveRight()
-
 proc createDom(): VNode =
   result = buildHtml(tdiv):
+    button():
+      text("New")
+      proc onclick() =
+        init()
     renderBoard()
-    renderController()
+
+proc onkeydown(ev: dom.Event) =
+  let ev = KeyboardEvent(ev)
+  case ev.key
+  of "ArrowLeft":
+    move(Left)
+  of "ArrowDown":
+    move(Down)
+  of "ArrowUp":
+    move(Up)
+  of "ArrowRight":
+    move(Right)
+  redraw(kxi)
+
+window.addEventListener("keydown", onkeydown)
+
+init()
 
 setRenderer(createDom)
